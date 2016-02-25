3d221cb8134b523c970020fdc07ae45f1df0c525
davenaugler <dv.naugler@gmail.com>
2016-02-24 19:37:05 -0700

    Commit

--- Month Two/MyRoster/app/controller/rosterController.js
+++ Month Two/MyRoster/app/controller/rosterController.js
@@ -1,4 +1,25 @@
-app.controller('RosterController', function($scope, DataService){
+app.controller('RosterController', function ($scope) {
+
+    //YOUR CODE HERE
     
+    $scope.players = [];
+
+    $scope.addPlayer = function () {      
+        var player = {
+            name: $scope.newPlayer.Name,
+            position: $scope.newPlayer.Position,
+            number: $scope.newPlayer.Number
+        }
+
+        $scope.players.push(player);
+        $scope.resetPlayer();
+    }
+    
+    $scope.resetPlayer = function()
+    {
+        $scope.newPlayer = "";
+        
+    }
+
 
 })
